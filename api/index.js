7da4242c52b01c83f9171a9baca761bb06cf62fa
MrSidSir <irshad1554@gmail.com>
2025-06-12 17:59:24 +0530

    #

--- api/index.js
+++ api/index.js
@@ -27,7 +27,7 @@ app.use(cookieParser());
 app.use(express.json());
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173' ,'https://sid-estate.vercel.app'],
   credentials: true,
 }));
 
