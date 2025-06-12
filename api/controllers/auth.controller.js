b111067f47382351ac9dddd3e935bcce57a8f2ac
MrSidSir <irshad1554@gmail.com>
2025-06-12 18:09:27 +0530

    #

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -35,7 +35,7 @@ export const signin = async (req, res, next) => {
       .cookie('access_token', token, {
         httpOnly: true,
         sameSite: 'Lax',
-         secure: false, // set true in production with HTTPS
+         secure: true, // set true in production with HTTPS
    
       })
       .json(rest); // ✅ return user without password
@@ -59,7 +59,7 @@ export const google = async (req, res, next) => {
   .cookie('access_token', token, {
     httpOnly: true,
     sameSite: 'Lax',
-    secure: false, // true if HTTPS
+    secure: true, // true if HTTPS
   })
   .json(rest);
     } else {
@@ -86,7 +86,7 @@ export const google = async (req, res, next) => {
   .cookie('access_token', token, {
     httpOnly: true,
     sameSite: 'Lax',
-    secure: false, // true if HTTPS
+    secure: true, // true if HTTPS
   })
   .json(rest);
     }
